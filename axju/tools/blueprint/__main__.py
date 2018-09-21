564cac7cd329ed65228dd2f26ee374c36b04fa10
axju <axel.juraske@short-report.de>
2018-09-21 15:06:07 +0200

    Add blueprint class

--- axju/tools/blueprint/__main__.py
+++ axju/tools/blueprint/__main__.py
@@ -1,29 +1,64 @@
 import argparse
 
+settings = {
+    'dirs': ['projects/', 'storage/music/', 'storage/mamorys/', 'storage/.../', ],
+    'progs': {
+        'git': {},
+        'thundbird': {},
+        'atom': {
+            'linux':[ {'cmd': ['sudo add-apt-repository ppa:webupd8team/atom', 'sudo apt update', 'sudo apt install atom']}],
+            'win': [{'exe': 'https://atom.io/download/windows_x64'}],
+        },
+    }
+}
+
+class Blueprint(object):
+    """docstring for Blueprint."""
+    def __init__(self, settings):
+        self.settings = settings
+
+    def __dirs(self):
+        return self.settings.get('dirs', [])
+
+    def __progs(self):
+        return self.settings.get('progs', [])
+
+    def show(self, kinds=['dirs', 'progs']):
+        """Display some information about your settings"""
+
+        if 'dirs' in kinds:
+            print("\nYour direcorys:")
+            for dir in self.__dirs():
+                print("  ", dir)
+
+        if 'progs' in kinds:
+            print("\nYour programms:")
+            for prog in self.__progs():
+                print("  ", prog)
+
+
+    def home(self, ):
+        """Setup your home folder"""
+        pass
+
+    def setup(self):
+        """Install all your programms"""
+        pass
+
+
+
+def structur():
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser()
-
-    subparsers = parser.add_subparsers(help='commands')
-
-    # A list command
-    list_parser = subparsers.add_parser('list', help='List contents')
-    list_parser.add_argument('dirname', action='store', help='Directory to list')
-
-    # A create command
-    create_parser = subparsers.add_parser('create', help='Create a directory')
-    create_parser.add_argument('dirname', action='store', help='New directory to create')
-    create_parser.add_argument('--read-only', default=False, action='store_true',
-                               help='Set permissions to prevent writing to the directory',
-                               )
-
-    # A delete command
-    delete_parser = subparsers.add_parser('delete', help='Remove a directory')
-    delete_parser.add_argument('dirname', action='store', help='The directory to remove')
-    delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
-                               help='Remove the contents of the directory, too',)
-
     #parser.parse_args()
-    parser.print_help()
+    #parser.print_help()
+
+    blueprint = Blueprint(settings)
+    blueprint.show()
+
 
 
 if __name__ == '__main__':
